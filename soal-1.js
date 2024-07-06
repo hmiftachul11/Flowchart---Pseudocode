24697a7d26cfb58e9f2c62c0b21c7ba4a6b3f1fa
hmiftachul11 <hmiftachul11@gmail.com>
2024-07-07 01:51:30 +0700

    Answer Task no 1

---
+++ soal-1.js
@@ -0,0 +1,17 @@
+function getGrade(nilai) {
+    let grade;
+    if (nilai < 60) {
+        grade = 'D';
+    } else if (nilai < 75) {
+        grade = 'C';
+    } else if (nilai < 85) {
+        grade = 'B';
+    } else if (nilai >= 85) {
+        grade = 'A';
+    }
+    return grade;
+}
+
+let nilai = prompt('Masukkan Nilai: ');
+let grade = getGrade(nilai);
+alert(`Nilai ${nilai} dinyatakan ${grade}`)
